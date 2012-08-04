8750c32d217561e23f70685e1f0c21e3d73dcc86
Fernando Lener <nando0205@gmail.com>
2012-08-04 20:08:03 -0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mc347Project::Application.config.secret_token = '637a29878240fb4c38a937f76c7006593a1a14ee28de3ed0ae902d82de26b63efef858e3b5b22a93d31038583ce9a92e4c125e8ae4c73b198baab24227ba9989'
